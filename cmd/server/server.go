bb27cb3a90282126ad921510be8581433321723a
Robert Boros <robbie13.boros@gmail.com>
2021-03-20 12:58:44 +0100

    add default values to flags

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,13 +4,13 @@ import (
 	"github.com/PumpkinSeed/heimdall/cmd/server/flags"
 	"github.com/PumpkinSeed/heimdall/internal/api/grpc"
 	"github.com/PumpkinSeed/heimdall/internal/api/rest"
-	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
+const serviceCount = 2
+
 var Cmd = &cli.Command{
 	Name:   "server",
-	Before: checkFlags,
 	Action: serve,
 	Flags: []cli.Flag{
 		flags.Grpc,
@@ -18,38 +18,18 @@ var Cmd = &cli.Command{
 	},
 }
 
-func checkFlags(ctx *cli.Context) error {
-	if ctx.NumFlags() == 0 {
-		log.Warn("Consider adding flags to turn on specific services, learn more about flags with --help")
-	}
-
-	return nil
-}
-
 func serve(ctx *cli.Context) error {
-	errCh := make(chan error)
-	services := run(ctx, errCh)
-	for services > 0 {
+	errCh := make(chan error, serviceCount)
+
+	grpc.Serve(errCh, ctx.String(flags.NameGrpc))
+	rest.Serve(errCh, ctx.String(flags.NameRest))
+
+	for i := 0; i < serviceCount; i++ {
 		err := <-errCh
 		if err != nil {
 			return err
 		}
-		services--
 	}
 
 	return nil
-}
-
-func run(ctx *cli.Context, errCh chan error) int {
-	var startedServices int
-	if grpcAddress := ctx.String(flags.NameGrpc); grpcAddress != "" {
-		grpc.Serve(errCh, grpcAddress)
-		startedServices++
-	}
-	if restAddress := ctx.String(flags.NameRest); restAddress != "" {
-		rest.Serve(errCh, restAddress)
-		startedServices++
-	}
-
-	return startedServices
 }
