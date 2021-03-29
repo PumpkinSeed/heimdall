e223262971ed853baed3d70cebb38d60f04ad29f
Robert Boros <robbie13.boros@gmail.com>
2021-03-29 11:18:56 +0200

    add socket to com layer

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,11 +4,10 @@ import (
 	"github.com/PumpkinSeed/heimdall/cmd/server/flags"
 	"github.com/PumpkinSeed/heimdall/internal/api/grpc"
 	"github.com/PumpkinSeed/heimdall/internal/api/rest"
+	"github.com/PumpkinSeed/heimdall/internal/api/socket"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
-
-const serviceCount = 2
 
 var Cmd = &cli.Command{
 	Name:   "server",
@@ -24,8 +23,9 @@ func serve(ctx *cli.Context) error {
 
 	serverExecutor(grpc.Serve, ctx.String(flags.NameGrpc), finished)
 	serverExecutor(rest.Serve, ctx.String(flags.NameRest), finished)
+	serverExecutor(socket.Serve, socket.Path, finished)
 
-	<- finished
+	<-finished
 
 	return nil
 }
@@ -36,5 +36,5 @@ func serverExecutor(fn func(string) error, str string, finisher chan struct{}) {
 			log.Error(err)
 		}
 		finisher <- struct{}{}
-	} ()
+	}()
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/PumpkinSeed/heimdall
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/golang/protobuf v1.5.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
@@ -11,4 +11,5 @@ require (
 	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6 // indirect
 	google.golang.org/grpc v1.36.0
+	google.golang.org/protobuf v1.26.0
 )
