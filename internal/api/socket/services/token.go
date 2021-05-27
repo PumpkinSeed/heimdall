33e337e93bbba446027006758d63bb729a20a551
Robert Boros <robbie13.boros@gmail.com>
2021-05-27 16:41:19 +0200

    remove errors package from token socket service

--- internal/api/socket/services/token.go
+++ internal/api/socket/services/token.go
@@ -3,8 +3,8 @@ package services
 import (
 	"context"
 	"encoding/json"
-	"errors"
 
+	"github.com/PumpkinSeed/heimdall/internal/errors"
 	"github.com/PumpkinSeed/heimdall/internal/structs"
 	"github.com/PumpkinSeed/heimdall/pkg/crypto/unseal"
 	"github.com/PumpkinSeed/heimdall/pkg/token"
@@ -26,11 +26,11 @@ func NewTokenCreate(u *unseal.Unseal) TokenCreate {
 
 func (t TokenCreate) Handler(ctx context.Context, req structs.SocketRequest) (structs.SocketResponse, error) {
 	if status := t.state.Status(); !status.Unsealed {
-		return structs.SocketResponse{}, errors.New("Heimdall sealed")
+		return structs.SocketResponse{}, errors.New("heimdall is still sealed", errors.CodeApiSocketTokenHandlerSealed)
 	}
 	var token token.Request
 	if err := json.Unmarshal(req.Data, &token); err != nil {
-		return structs.SocketResponse{}, err
+		return structs.SocketResponse{}, errors.Wrap(err, "token request unmarshal error", errors.CodeApiSocketTokenHandlerUnmarshal)
 	}
 	if rootTokenValid, err := t.tokenStore.CheckToken(ctx, token.RootTokenID); err != nil {
 		return structs.SocketResponse{}, err
@@ -42,9 +42,9 @@ func (t TokenCreate) Handler(ctx context.Context, req structs.SocketRequest) (st
 	rootToken, err := t.tokenStore.GenRootToken(ctx, token.ID)
 	data, err := json.Marshal(rootToken)
 	if err != nil {
-		return structs.SocketResponse{}, err
+		return structs.SocketResponse{}, errors.Wrap(err, "token request marshal error", errors.CodeApiSocketTokenHandlerMarshal)
 	}
 	return structs.SocketResponse{
 		Data: data,
-	}, err
+	}, nil
 }
